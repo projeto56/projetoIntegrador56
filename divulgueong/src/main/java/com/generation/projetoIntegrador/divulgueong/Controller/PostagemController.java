88c748935530402abb13a11f8c34dfb24dcc0c0b
Rafael Silveira <rafaell.ssouza@hotmail.com>
2022-08-03 15:08:22 -0300

    criado a função getAll e getId

---
+++ divulgueong/src/main/java/com/generation/projetoIntegrador/divulgueong/Controller/PostagemController.java
@@ -0,0 +1,5 @@
+package com.generation.projetoIntegrador.divulgueong.Controller;
+
+public class PostagemController {
+
+}
---
+++ divulgueong/src/main/java/com/generation/projetoIntegrador/divulgueong/Repository/PostagemRepository.java
@@ -0,0 +1,13 @@
+package com.generation.projetoIntegrador.divulgueong.Repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.generation.projetoIntegrador.divulgueong.model.Postagem;
+
+public interface PostagemRepository extends JpaRepository<Postagem, Long>{
+
+	public List<Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo")String titulo);
+}
