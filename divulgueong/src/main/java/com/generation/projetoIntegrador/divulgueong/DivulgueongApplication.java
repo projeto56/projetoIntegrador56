74428324d34344d2e99e7a27b55fe69a7da5258d
Rafael Silveira <rafaell.ssouza@hotmail.com>
2022-08-03 11:53:23 -0300

    criado projeto com spring boot

---
+++ divulgueong/src/main/java/com/generation/projetoIntegrador/divulgueong/DivulgueongApplication.java
@@ -0,0 +1,13 @@
+package com.generation.projetoIntegrador.divulgueong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DivulgueongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DivulgueongApplication.class, args);
+	}
+
+}
---
+++ divulgueong/src/test/java/com/generation/projetoIntegrador/divulgueong/DivulgueongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.projetoIntegrador.divulgueong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DivulgueongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
