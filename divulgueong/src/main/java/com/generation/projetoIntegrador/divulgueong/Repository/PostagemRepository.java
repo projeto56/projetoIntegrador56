7f9258c96514ffc5d80f50d250cc2c47290df7bd
Rafael Silveira <rafaell.ssouza@hotmail.com>
2022-08-03 15:25:20 -0300

    criado a função post e put da postagem

--- divulgueong/src/main/java/com/generation/projetoIntegrador/divulgueong/Repository/PostagemRepository.java
+++ divulgueong/src/main/java/com/generation/projetoIntegrador/divulgueong/Repository/PostagemRepository.java
@@ -10,4 +10,6 @@ import com.generation.projetoIntegrador.divulgueong.model.Postagem;
 public interface PostagemRepository extends JpaRepository<Postagem, Long>{
 
 	public List<Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo")String titulo);
+	
+	public List<Postagem> findAllByAutorContainingIgnoreCase(@Param("autor")String autor);
 }
