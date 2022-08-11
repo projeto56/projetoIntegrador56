0c4c5627785d0689e5998d5a67995da9a8ee5319
Vagner <vagner-bw@hotmail.com>
2022-08-11 14:37:25 -0300

    Criando Pacote Security

--- divulgueong/src/main/java/com/generation/projetoIntegrador/divulgueong/Repository/UsuarioRepository.java
+++ divulgueong/src/main/java/com/generation/projetoIntegrador/divulgueong/Repository/UsuarioRepository.java
@@ -1,9 +1,8 @@
 package com.generation.projetoIntegrador.divulgueong.Repository;
 
-import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.generation.projetoIntegrador.divulgueong.model.Usuario;
@@ -11,6 +10,6 @@ import com.generation.projetoIntegrador.divulgueong.model.Usuario;
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 	
-	public List<Usuario>findAllByNomeContainingIgnoreCase(@Param("nome")String nome);
+	public Optional<Usuario>findAllByUsuario (String usuario);
 
 }
---
+++ divulgueong/src/main/java/com/generation/projetoIntegrador/divulgueong/Security/UserDetailsServiceImpl.java
@@ -0,0 +1,38 @@
+package com.generation.projetoIntegrador.divulgueong.Security;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.generation.projetoIntegrador.divulgueong.Repository.UsuarioRepository;
+import com.generation.projetoIntegrador.divulgueong.model.Usuario;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+	
+	@Autowired
+	private UsuarioRepository repository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+		
+		Optional<Usuario> usuario = repository.findByUsuario(userName);
+		
+		usuario.orElseThrow(() -> new UsernameNotFoundException(userName +
+				" não encontrado."));
+		
+		return usuario.map(UserDetailsImpl :: new).get();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
