4121d3612eb98a11d4c3c2a682f67abe3fb48c67
Jonathan Rodrigues <jonathan.rf.sousa@gmail.com>
2022-08-17 09:15:14 -0300

    teste

--- divulgueong/src/main/java/com/generation/projetoIntegrador/divulgueong/Controller/UsuarioController.java
+++ divulgueong/src/main/java/com/generation/projetoIntegrador/divulgueong/Controller/UsuarioController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -53,7 +54,7 @@ public class UsuarioController {
 				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
 				}
 	
-	@GetMapping("/cadastrar")
+	@PostMapping("/cadastrar")
 	public ResponseEntity<Usuario> postUsuario
 	(@RequestBody @Valid Usuario usuario){
 		return service.cadastrarUsuario(usuario)
